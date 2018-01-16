d5911e1d67c392f6bbaae0bc82c68be7dc5c58a9
UnHumbleBen <benjaminsll18@gmail.com>
2018-01-16 11:58:37 -0800

    Added color and ellipse

--- MyFirstBox.java
+++ MyFirstBox.java
@@ -1,16 +1,32 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import java.awt.Color;
+
+/*
+class CircleComponent extends JComponent {
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        Ellipse2D.Double circle = new Ellipse2D.Double(250,250,50,150);
+        g2.draw(circle);
+    }
+}
+*/
 
 class RectangleComponent extends JComponent {
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        
-        Rectangle box = new Rectangle(35,40,50,150);
+        Rectangle box = new Rectangle(50,50,50,150);
+        g2.setColor(Color.ORANGE);
         g2.draw(box);
-        
+        g2.setColor(Color.RED);
+        g2.fill(box);
+        g2.drawString("My first rectangle", 50,20); 
+        Ellipse2D.Double circle = new Ellipse2D.Double(250,250,50,150);
+        g2.draw(circle);
     }
 }
 
@@ -22,7 +38,9 @@ public class MyFirstBox {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         RectangleComponent myBox = new RectangleComponent();
         frame.add(myBox);
-        
         frame.setVisible(true);
+        //CircleComponent myCircle = new CircleComponent();
+        //frame.add(myCircle);
+        //frame.setVisible(true);
     }
 }
