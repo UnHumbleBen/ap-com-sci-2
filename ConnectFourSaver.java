9e09bde9c46e3478d54ab961595470b918cdfbe9
UnHumbleBen <benjaminsll18@gmail.com>
2018-01-19 17:14:20 -0800

    Color of circles alternate correctly

--- ConnectFourSaver.java
+++ ConnectFourSaver.java
@@ -7,9 +7,16 @@ import javax.swing.JFrame;
 import java.awt.Color;
 import java.awt.event.*;
 class GridComponent extends JComponent {
-    boolean isYellow = true;
+    boolean isYellowTurn = false; //initalizes players turns for coloring purposes
+    
+    //dimensions for box
     final int ROWS = 6;
     final int COLUMNS = 7;
+    final int DIAMETER = 50;
+    final int BOX_WIDTH = 2*DIAMETER * COLUMNS;
+    final int BOX_HEIGHT =  2*DIAMETER * ROWS;
+    
+    //set up array of circle to keep track of status (filled or not filled/ color/ etc.)
     public Circle[][] circles = new Circle[ROWS][COLUMNS];
     public void createCircles() {
         for (int k = 0; k < ROWS; k++) {
@@ -18,52 +25,62 @@ class GridComponent extends JComponent {
             }
         }
     }
-
+    
+    // returns a specific circle (given a row and column) (Used in paint component)
     public  Circle getCircles(int r, int c) {
         return circles[r][c];
     }
 
     public void paintComponent(Graphics g) {
-
-        final int DIAMETER = 50;
-        final int BOX_WIDTH = 2*DIAMETER * COLUMNS;
-        final int BOX_HEIGHT =  2*DIAMETER * ROWS;
-
         Graphics2D g2 = (Graphics2D) g;
+        
+        //draws and fills box
         Rectangle box = new Rectangle(0,0,BOX_WIDTH,BOX_HEIGHT);
         g2.setColor(Color.BLUE);
         g2.fill(box);
         g2.draw(box);
 
-        //Circle[][] circles = new Circle[ROWS][COLUMNS];
+        //draws and fills circles
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLUMNS; j++) {
                 Circle test = circles[i][j];
-                if (!test.isFilled()) {
+                if (!test.isFilled()) {// for empty circles
                     g2.setColor(Color.WHITE);   
                 }
-                else if(isYellow) {
-                    g2.setColor(Color.YELLOW); 
-                    isYellow = !isYellow;
+                else if(!test.isColorDecided()) { // when user click on circle
+                    g2.setColor(Color.YELLOW);
+                    test.ColorDecided(); //finalizes color for a circle
+                    // default color is yellow
+                    if (!isYellowTurn) {
+                        g2.setColor(Color.RED);
+                        test.setRed(); // changes to color to red on red player turn
+                    }
                 }
-                else {
-                    g2.setColor(Color.RED); 
-                    isYellow = !isYellow;
+                else { // for already decided(finalized) circles
+                    g2.setColor(Color.YELLOW); //default color is yellow
+                    if(!test.isYellow()) {// for red circles
+                        g2.setColor(Color.RED);
+                    }
                 }
                 Ellipse2D.Double circle = new Ellipse2D.Double(2*j*DIAMETER + DIAMETER/2,2*i*DIAMETER + DIAMETER/2,DIAMETER,DIAMETER);
                 g2.fill(circle);
                 g2.draw(circle);
             }
         }
+        
+        isYellowTurn = !isYellowTurn; // switch player's turn (for coloring purpose)
     }
 }
 
+//Circle class keeps track of position, vacancy, and color of a specific circle
 class Circle {
-    private boolean filled = false;
-    private int row;
-    private int column;
+    private boolean filled = false; // true if circle is already colored yellow or red
+    private boolean isColorDecided = false; //true if circle color had already decided in an earlier turn
+    private boolean isYellow = true; //default color is yellow
+    private int row; // row in the board
+    private int column; // column in the board
 
-    public Circle(int r, int c) {
+    public Circle(int r, int c) {// constructs circles given position (used in createCircles())
         row = r;
         column = c;
     }
@@ -72,16 +89,25 @@ class Circle {
         return filled;   
     }
 
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
     public void setFilled() {
         filled = true;
+    }
+    // sets color to red (instead of the default yellow)
+    public void setRed() {
+        isYellow = false;
+    }
+    
+    public boolean isYellow() {
+        return isYellow;
+    }
+    
+    public boolean isColorDecided() {
+        return isColorDecided;
+    }
+    
+    // prevents color from changing
+    public void ColorDecided() {
+        isColorDecided = true;
     }
 }
 
@@ -91,29 +117,26 @@ public class ConnectFourSaver {
         frame.setSize(750,650);
         frame.setTitle("Connect Four");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        GridComponent myBox = new GridComponent();
-        myBox.createCircles();
+        GridComponent C4 = new GridComponent(); //initalizes the board
+        C4.createCircles(); // creates circle classes 
+        
         class MouseClickListener implements MouseListener {
             public void mousePressed(MouseEvent event){}
-
             public void mouseExited(MouseEvent event){}
-
             public void mouseEntered(MouseEvent event){}
-
             public void mouseReleased(MouseEvent event){}
 
             public void mouseClicked(MouseEvent event) {
-                int x = event.getX()/100; //returns x coordinate mouse clicked locaction
-                int y = event.getY()/100; //returns y coordinate
-                myBox.getCircles(y,x).setFilled();
-                myBox.repaint();
-                System.out.println("this works");
+                int x = event.getX()/100; //to find column
+                int y = event.getY()/100; // to find row
+                C4.getCircles(y,x).setFilled();
+                C4.repaint();
             }
         }
         MouseClickListener listener = new MouseClickListener();
-        myBox.addMouseListener(listener);
+        C4.addMouseListener(listener);
 
-        frame.add(myBox);
+        frame.add(C4);
         frame.setVisible(true);
     }
 }
