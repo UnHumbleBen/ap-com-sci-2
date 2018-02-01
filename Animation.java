7ccc69a3e6189572ba7123154721d1edfc77526f
UnHumbleBen <benjaminsll18@gmail.com>
2018-02-01 12:33:13 -0800

    something wads done

--- Animation.java
+++ Animation.java
@@ -8,31 +8,50 @@ import java.awt.Color;
 
 
 class Bubble extends JComponent{
+    private int x = 0;
+    private int y = 0;
+    private boolean right = true;
+    private boolean down = true;
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        Rectangle box = new Rectangle(50,50,50,150);
-        g2.setColor(Color.ORANGE);
-        g2.draw(box);
-        g2.setColor(Color.RED);
-        g2.fill(box);
-        g2.drawString("My first rectangle", 50,20); 
-        Ellipse2D.Double circle = new Ellipse2D.Double(250,250,50,150);
+        Ellipse2D.Double circle = new Ellipse2D.Double(x,y,50,50);
+        Rectangle back = new Rectangle(0,0,500,500);
+        if (right) {
+            x += 50;
+            //y += 50;
+        }
+        else {
+            x -= 50;
+            //y -= 50;
+        }
+        
+        if (down) {
+            y += 40;
+        }
+        else {
+            y -= 40;
+        }
+        if (x >= 450 || x <= 0) right = !right;
+        if (y >= 450 || y <= 0) down = !down;
+        g2.draw(back);
         g2.draw(circle);
     }
 }
 public class Animation
 {
-    public static void main() {
+    public static void main(String[] args) throws InterruptedException{
         JFrame frame = new JFrame();
         frame.setSize(500,500);
         frame.setTitle("Smiley");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        RectangleComponent myBox = new RectangleComponent();
+        Bubble myBox = new Bubble();
         frame.add(myBox);
-        for (int i = 0; i < 10; i++) {
-            
+        while (true) {
+            Thread.sleep(500);
+            myBox.repaint();
+            frame.setVisible(true);
         }
         
-        frame.setVisible(true);
+        //frame.setVisible(true);
     }
 }
