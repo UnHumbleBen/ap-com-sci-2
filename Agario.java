1b5d10471428d8a398893f193fdfcc65a90a760a
UnHumbleBen <benjaminsll18@gmail.com>
2018-02-10 17:42:23 -0800

    Eating update

--- Agario.java
+++ Agario.java
@@ -47,7 +47,7 @@ class Prey {
     public void xCollision() {
         isRight = !isRight;
         xVel = random.nextInt(9)+1;
-
+        
         if (!isRight) {
             xVel = -1*xVel;
             xPos = 400-diameter;
@@ -73,15 +73,26 @@ class Prey {
         xPos += xVel;
         yPos += yVel;
     }
-    
+
 }
 
 class Predator extends Prey{
     public Predator(int newDiameter) {
         super(newDiameter);
     }
+
     public void eats(Prey p) {
+        System.out.println("Eat");
+        System.out.println("Position before eating: (" + getX() + "," + getY() + ")");
+        System.out.println("Center Position before eating: (" + ((getX() + getDiameter()/2)) + "," + (getY() + getDiameter()/2) + ")");
+        int x1 = getX();
+        int x2 = getY();
         diameter += p.getDiameter();
+        setX(getX() - p.getDiameter()/2);
+        setY(getY() - p.getDiameter()/2);
+        System.out.println("Position after eating: (" + getX() + "," + getY() + ")");
+        System.out.println("Center Position after eating: (" + ((getX() + getDiameter()/2)) + "," + (getY() + getDiameter()/2) + ")");
+        System.out.println("Center differennce before and after: (" + (getX() - x1) + "," + (getY() -x2 ) + ")");  
     }
 }
 
@@ -104,7 +115,12 @@ class Micro extends JComponent {
         Rectangle blueButton = new Rectangle(frameWidth + 10, frameHeight/2 - 60, 50, 50);
         g2.draw(blueButton);
         g2.fill(blueButton);
-
+        
+        g2.setColor(Color.GREEN);
+        Rectangle greenButton = new Rectangle(frameWidth + 10, frameHeight/2, 50, 30);
+        g2.draw(greenButton);
+        g2.fill(greenButton);
+        
         for (int i = 0; i < predators.size(); i++) {
             Predator predator = predators.get(i);
             int leftBound = predator.getX();
@@ -112,14 +128,33 @@ class Micro extends JComponent {
             int topBound = predator.getY();
             int bottomBound = predator.getY() + predator.getDiameter();
             for (int j = 0; j < preys.size(); j++) {
-               Prey prey = preys.get(j);
-               int preyX = prey.getX();
-               int preyY = prey.getY();
-               int preyDiameter = prey.getDiameter();
-               if (preyX >= leftBound && preyX <= rightBound - preyDiameter && preyY >= topBound && preyY <= bottomBound - preyDiameter) {
-                   predator.eats(prey);
-                   preys.remove(prey); 
-               }
+                Prey prey = preys.get(j);
+                int preyX = prey.getX();
+                int preyY = prey.getY();
+                int preyDiameter = prey.getDiameter();
+                if (preyX >= leftBound && preyX <= rightBound - preyDiameter && preyY >= topBound && preyY <= bottomBound - preyDiameter) {
+                    predator.eats(prey);
+                    preys.remove(prey); 
+                }
+            }
+
+            for (int k = 0; k < predators.size(); k++) {
+                Predator predator2 = predators.get(k);
+                int predator2X = predator2.getX();
+                int predator2Y = predator2.getY();
+                int predator2D = predator2.getDiameter();
+
+                if (predator2X >= leftBound && predator2X <= rightBound - predator2D && predator2Y >= topBound && predator2Y <= bottomBound - predator2D) {
+                    if (predator.getDiameter() > predator2D) {
+                        predator.eats(predator2);
+                        predators.remove(predator2); 
+                    }
+                    else if(predator.getDiameter() < predator2D) {
+                        predator2.eats(predator);
+                        predators.remove(predator);
+                    }
+                }
+
             }
         }
 
@@ -152,6 +187,11 @@ class Micro extends JComponent {
             g2.fill(newPredator);
             g2.draw(newPredator);
         }
+    }
+    
+    public void resetAll() {
+        preys.clear();
+        predators.clear();
     }
 }
 
@@ -187,6 +227,10 @@ public class Agario
                     Predator newPredator = new Predator(50);
                     myAgario.predators.add(newPredator);
                 }
+                
+                if (x >= 410 && x <= 460 && y >= 200 && y <= 230) {
+                    myAgario.resetAll();
+                }
             }
         }
         MouseClickListener listener = new MouseClickListener();
@@ -194,7 +238,7 @@ public class Agario
 
         frame.add(myAgario);
         while (true) {
-            Thread.sleep(10);
+            Thread.sleep(50);
             myAgario.repaint();
             frame.setVisible(true);
         }
