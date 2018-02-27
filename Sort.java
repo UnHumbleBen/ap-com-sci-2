f06ded5458af51969b8fc13c6c97d8f1cd583dfe
UnHumbleBen <benjaminsll18@gmail.com>
2018-02-27 15:50:54 -0800

    First commit

--- Sort.java
+++ Sort.java
@@ -31,6 +31,13 @@ public class Sort
         System.out.println();
     }
     
+    /** Swaps the elements in the  array  at index  a  and index  b  .*/
+    public void swap(int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+    
     /** Sorts array in order from least to greatest
      *  Uses bubble sort.
      */
@@ -38,9 +45,7 @@ public class Sort
         for (int i = 0; i < array.length-1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j+1]) {
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    swap(j,j+1);
                 }
             }
         }
@@ -50,13 +55,30 @@ public class Sort
      *  Uses selection sort.
      */
     public void selectionSort() {
-        
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minIndex = i;
+            for (int j = i; j < array.length ; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minIndex = j;
+                }
+            }
+            swap(i, minIndex);
+        }
     }
     
     public static void main(String[] args) {
+        System.out.println("Bubble sort");
         Sort b = new Sort(10);
         b.printArray();
         b.bubbleSort();
         b.printArray();
+        
+        System.out.println("Selection sort");
+        Sort c = new Sort(10);
+        c.printArray();
+        c.selectionSort();
+        c.printArray();
     }
 }
---
+++ SortAnimation.java
@@ -0,0 +1,28 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.event.*;
+import java.util.*;
+
+class Component extends JComponent {
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g; 
+    }
+}
+
+
+public class SortAnimation {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(1000,1000);
+        frame.setTitle("Benjamin Lee");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Component mySort = new Component();
+        frame.add(mySort);
+        frame.setVisible(true);
+    }
+}
